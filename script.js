411bce2ccdab8057b5402edc9d978546bea2aeae
Brian Monteverde <bmonteve@gmail.com>
2020-01-29 14:39:32 -0800

    Added comments explaining script functionality

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
+//Creates and tracks current date and hour, while also showing that on the browser
 var date = moment().format('dddd, MMMM Do');
 var keyDate = moment().get('date');
 var currentHour = moment().hour();
 $("#currentDay").text(date);
 
-
+//displays blocks for 7am to 7p on the browser
 for (index = 7; index < 20; index++){
     $(".container").append(makeBlocks(index));
 }
 
+//first call to run time and update row colors 
 updateTime();
 updateRow();
 
+//interval to update time and row colors in real time w/o requirement for a page refresh
 setInterval(function(){
     updateTime();
     updateRow();
 }, 1000);
 
+//sets current day events to local storage, and clears out storage when a new day begins
+/*will adjust functionality of the local storage calls when next and previous day buttons
+are made functional*/
 if (keyDate != localStorage.getItem("date")){
     localStorage.clear();
     localStorage.setItem("date", keyDate);
 }
 
+//creates the indivual rows for each hour of my current work day 
 function makeBlocks (i){
     var hour = $("<div>", {"class": "time-block row"});
     var time = $("<span>", {"class": "hour"});
@@ -47,6 +54,7 @@ function makeBlocks (i){
     return hour;
 }
 
+//adds a past, present, or future class to adjust the css to the row based on the current hour
 function hourBack(){
     var index = parseInt($(this).attr("val"));
     currentHour = moment().hour();
@@ -66,14 +74,18 @@ function hourBack(){
     }
 }
 
+//works with the interval to check each element with the row class to update the background color
+//calls hourBack for each element to adjust the classes
 function updateRow (){
     $(".row").each(hourBack);
 }
 
+//works with interval to adjust time on page without a refresh
 function updateTime(){
     $("#currentTime").text(moment().format("LTS"));
 }
 
+//adds functionality to the save buttons for each row, is disable when row has class of past
 $(".saveBtn").on("click", function (){
     var time = $(this).parent().attr("val");
     var event = $(this).parent().find("textArea").val()
