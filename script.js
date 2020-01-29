de1dbbb27e65623e646d6b0659b756a726ee156f
Brian Monteverde <bmonteve@gmail.com>
2020-01-29 14:05:03 -0800

    fixed issue with coloring rows based on time, added previous and next day buttons

--- script.js
+++ script.js
@@ -3,29 +3,22 @@ var keyDate = moment().get('date');
 var currentHour = moment().hour();
 $("#currentDay").text(date);
 
-updateTime();
-
-setInterval(function(){
-    updateTime();
-}, 1000);
-
-
-if (keyDate != localStorage.getItem("date")){
-    localStorage.clear();
-    localStorage.setItem("date", keyDate);
-}
 
 for (index = 7; index < 20; index++){
     $(".container").append(makeBlocks(index));
 }
 
-function hourBack(i){
-    if (currentHour > i)
-        return "past";
-    else if (currentHour < i)
-        return "future";
-    else 
-        return "present";
+updateTime();
+updateRow();
+
+setInterval(function(){
+    updateTime();
+    updateRow();
+}, 1000);
+
+if (keyDate != localStorage.getItem("date")){
+    localStorage.clear();
+    localStorage.setItem("date", keyDate);
 }
 
 function makeBlocks (i){
@@ -35,42 +28,46 @@ function makeBlocks (i){
     var icon = $("<i>", {"class": "material-icons"});
     var save = $("<button>", {"class": "saveBtn"});
 
+    if(i<12)
+        time.text(i + " AM");
+    else if (i == 12)
+        time.text((i) + " PM");
+    else
+        time.text((i-12) + " PM");
+
+    hour.attr("val", i);
+    event.val(localStorage.getItem(i));
     icon.html("assignment_returned");
     save.append(icon);
 
-    if(i<12){
-        time.text(i + " AM");
-        hour.attr("val", i);
-        event.addClass (hourBack(i));
-        event.val(localStorage.getItem(i));
-        hour.append(time);
-        hour.append(event);
-        if (event.hasClass("past")){
-            save.attr("disabled", true);
-            save.attr("style", "color: rgb(131, 188, 201)");
-        }
-        hour.append(save);
-    }
-    else{
-        hour.attr("val", (i-12));
-        event.addClass (hourBack(i));
-        event.val(localStorage.getItem(i-12));
-        
-        if (i == 12)
-            time.text((i) + " PM");
-        else{
-            time.text((i-12) + " PM");
-        }
-        if (event.hasClass("past")){
-            save.attr("disabled", true);
-            save.attr("style", "color: rgb(131, 188, 201)");
-        }
-        hour.append(time);
-        hour.append(event);
-        hour.append(save);
-    }
+    hour.append(time);
+    hour.append(event);
+    hour.append(save);
     
     return hour;
+}
+
+function hourBack(){
+    var index = parseInt($(this).attr("val"));
+    currentHour = moment().hour();
+    
+    if (currentHour > index){
+        $(this).removeClass("present");
+        $(this).addClass("past");
+        $(this).find("i").addClass("disabled");
+        $(this).find(".saveBtn").attr("disabled",true);
+    }
+    else if (currentHour < index){
+        $(this).addClass("future");
+    }
+    else {
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+}
+
+function updateRow (){
+    $(".row").each(hourBack);
 }
 
 function updateTime(){
@@ -82,3 +79,14 @@ $(".saveBtn").on("click", function (){
     var event = $(this).parent().find("textArea").val()
     localStorage.setItem(time, event);
 })
+
+//ADD Dom elements, Make the event row a button to bring up pop up
+    //Add edit button to show form input/edit details
+    //Have event row only show event title
+    //Make past rows uneditable
+
+//Add functionality to buttons to change the date
+    //Make past dates viewable but uneditable
+    //reformat LocalStorage use to data from events on multiple days
+    //eventually add calendar format to choose date instead of back and forward buttons
+
